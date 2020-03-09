ce791701b0fc8af6c0ac1c722d8a1bd55e17f919
Khaylarooks <krooks20@uw.edu>
2020-03-09 15:57:47 -0700

    added widgets

--- Khayla's/server.R
+++ Khayla's/server.R
@@ -7,51 +7,20 @@
 #    http://shiny.rstudio.com/
 #
 library(shiny)
-library(ECharts2Shiny)
 
-server <- function(input, output) {
-    hiv_df2 <- read.csv("data/by_area_hiv_treatment_est_from_1990-present.csv", stringsAsFactors = FALSE)
-    hiv_df2 <- hiv_df2[-(1:4),]
-    colnames(hiv_df2)<-hiv_df2[1,]
-    colnames(hiv_df2)[1:2] <- c("Year","Location")
-    hiv_df2 <- hiv_df2[-1,]
-    names(hiv_df2)[3] <- "Region"
-    hiv_df2_noNA <- hiv_df2[-c(20:50)]
-    names(hiv_df2_noNA)[4] <- "Percent of people receiving ART"
-    receiving <- hiv_df2_noNA %>%
-        filter(Year == "2018") %>%
-        select(Year, Region, `Percent of people receiving ART`) %>%
-        top_n(10)
-    name <- c("Eswatini", "Madagascar", "Namibia", " Rwanda","Zimbabwe", "Jordan",
-              "Cape Verde", "Denmark", "Italy", "Portugal", "Spain")
-    num <- c(86, 9, 92, 87, 88, 84, 89, 89, 91, 90, 84)
-    
-    output$example_plot <- renderPlotly({
-        receiving_therapy <- barplot(
-            num, names.arg = name, xlab = "Region", ylab = "People receiving HAART therapy", 
-            col = "blue", main = "Percent of people Receiving HAART")
-        
-        return(receiving_therapy)
-    })
-    output$out_message_2 <- renderText({
-        msg <- paste("", input$username, "!")
-        return(msg)
-    })
-    
-    output$example_plot_2 <- renderPlotly({
-        most_accessible <- ggplot(receiving) +
-            geom_point(mapping = aes(x = Region, y = `Percent of people receiving ART`)) +
-            geom_smooth (
-                mapping = aes(x = Region, y = `Percent of people receiving ART`),
-            )
-        return(ggplotly(most_accessible))
-    })
-    
-my ui    
-
-
+## Ui.R
+fluidPage(
+   selectInput("select", label = h3("Select Country"), 
+              choices = list("Middle East and North Africa" = 1, "Gambia" = 2, "Honduras" = 3, "Italy" = 4,
+                             "Myanmar" = 6, "Nepal" = 7, "Paraguay" = 8, "Somalia" = 9, "South Sudan" = 10), 
+              selected = 1),
+   hr(),
+  fluidRow(column(3, verbatimTextOutput("value")))
   
-  
-    
-    
-    
+)
+
+## Server
+
+function(input, output) {
+  output$value <- renderPrint({ input$select })
+}
